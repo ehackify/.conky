1d715bd276be6f4fa7a1c2aafe40d930c0f18644
Prasanthc41m <58906808+prasanthc41m@users.noreply.github.com>
2020-05-08 20:01:47 +0530

    Update install_conky.sh

--- install_conky.sh
+++ install_conky.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 #Conky for eHackify
 apt install conky -y
+#
 rm -rf ~/.conky
 git clone https://github.com/prasanthc41m/.conky.git
 chmod -R +x ~/.conky/*
+#
 crontab <<EOF
 @reboot /root/.conky/conky-startup.sh
 EOF
-sleep 05
-bash ~/.conky/conky-startup.sh 
+#
+bash ~/.conky/conky-startup.sh &
 echo "Conky will start automatically on every reboot!!!"
-exit && exit
+
 
 
