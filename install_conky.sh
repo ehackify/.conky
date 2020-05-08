da8cda4e20f54f66b1e55e5e238f04264d90582e
Prasanthc41m <58906808+prasanthc41m@users.noreply.github.com>
2020-05-08 17:22:02 +0530

    Update install_conky.sh

--- install_conky.sh
+++ install_conky.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Conky for eHackify
 apt install conky -y
-git clone https://github.com/prasanthc41m/conky.git
+git clone https://github.com/prasanthc41m/.conky.git
 bash ~/.conky/conky-startup.sh
 #
 
