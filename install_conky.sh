44a5c0032db2516820ec807edce7d89b7a8c26b3
Prasanthc41m <58906808+prasanthc41m@users.noreply.github.com>
2020-05-08 19:55:13 +0530

    Update install_conky.sh

--- install_conky.sh
+++ install_conky.sh
@@ -8,7 +8,8 @@ crontab <<EOF
 @reboot /root/.conky/conky-startup.sh
 EOF
 sleep 05
-bash ~/.conky/conky-startup.sh && sleep 05 && exit
+bash ~/.conky/conky-startup.sh && sleep 05
 echo "Conky will start automatically on every reboot!!!"
+exit
 
 
