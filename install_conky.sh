07797243f739d4236bf28130dad66b954ece0b3d
Prasanthc41m <58906808+prasanthc41m@users.noreply.github.com>
2020-05-08 18:57:51 +0530

    Update install_conky.sh

--- install_conky.sh
+++ install_conky.sh
@@ -4,7 +4,10 @@ apt install conky -y
 rm -rf .conky
 git clone https://github.com/prasanthc41m/.conky.git
 chmod +x ~/.conky/*
-bash ~/.conky/conky-startup.sh
+crontab <<EOF
+@reboot /root/.conky/conky-startup.sh
+EOF
+echo "Please reboot to start!!!" 
 #
 
 
